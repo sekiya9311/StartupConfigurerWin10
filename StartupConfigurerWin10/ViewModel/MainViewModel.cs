669e48ab8a3f0adb79f018f1fd2b7c9a2e1eed56
sekiya9311 <sekiya9311@gmail.com>
2019-03-14 00:17:21 +0900

    Add MainWindowViewModel (without impl😋)

---
+++ StartupConfigurerWin10/ViewModel/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using StartupConfigurerWin10.Model;
+
+namespace StartupConfigurerWin10.ViewModel
+{
+    class MainViewModel : ViewModelBase
+    {
+        public ObservableCollection<Shortcut> StartupShortcuts { get; set; }
+
+        private CommandBase _addCommand = null;
+        public CommandBase AddCommand =>
+            (_addCommand ?? (_addCommand = new CommandBase(Add)));
+
+        private CommandBase _removeCommand = null;
+        public CommandBase RemoveCommand =>
+            (_removeCommand ?? (_removeCommand = new CommandBase(Remove)));
+
+        private CommandBase _saveCommand = null;
+        public CommandBase SaveCommand =>
+            (_saveCommand ?? (_saveCommand = new CommandBase(Save)));
+
+        public MainViewModel()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            // TODO: get current startup shortcuts
+            this.StartupShortcuts = new ObservableCollection<Shortcut>();
+        }
+
+        private void Add()
+        {
+            // TODO: impl
+        }
+
+        private void Remove()
+        {
+            // TODO: impl
+            // remove selected items
+        }
+
+        private void Save()
+        {
+            // TODO: save startup
+        }
+    }
+}
