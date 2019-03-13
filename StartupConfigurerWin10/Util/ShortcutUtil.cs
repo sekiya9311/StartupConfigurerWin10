c271dc0da2b1bd39b37aee8a6aafcd2a78d54ca0
sekiya9311 <sekiya9311@gmail.com>
2019-03-14 00:54:07 +0900

    Add ShotcutUtil class (without impl too 😋)

---
+++ StartupConfigurerWin10/Util/ShortcutUtil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using StartupConfigurerWin10.Model;
+
+namespace StartupConfigurerWin10.Util
+{
+    class ShortcutUtil
+    {
+        public static string StartupPath => System.IO.Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.StartMenu),
+            "Programs",
+            "Startup");
+
+        public static IEnumerable<Shortcut> GetCurrentStartup()
+        {
+            // TODO: impl
+            throw new NotImplementedException();
+        }
+
+        public static void SaveStartup(IEnumerable<Shortcut> shortcuts)
+        {
+            // TODO: impl
+            // delete & insert ???
+        }
+    }
+}
