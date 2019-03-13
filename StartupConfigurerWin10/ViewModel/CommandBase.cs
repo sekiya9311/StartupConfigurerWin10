c68a274e10b9e0e4b8cf08ef378742983f60dea5
sekiya9311 <sekiya9311@gmail.com>
2019-03-13 23:24:30 +0900

    Add base class

---
+++ StartupConfigurerWin10/ViewModel/CommandBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace StartupConfigurerWin10.ViewModel
+{
+    class CommandBase : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private readonly Action act;
+
+        public CommandBase(Action _act)
+        {
+            this.act = _act;
+        }
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter) => this.act?.Invoke();
+    }
+}
---
+++ StartupConfigurerWin10/ViewModel/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace StartupConfigurerWin10.ViewModel
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(string param) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(param));
+    }
+}
