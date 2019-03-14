6736c33278e4a951ea92528596536cc7162eff49
sekiya9311 <sekiya9311@gmail.com>
2019-03-15 01:15:51 +0900

    Fix mistake

--- StartupConfigurerWin10/Util/ShortcutUtil.cs
+++ StartupConfigurerWin10/Util/ShortcutUtil.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using StartupConfigurerWin10.Model;
+using StartupConfigurerWin10.Entity;
 
 namespace StartupConfigurerWin10.Util
 {
