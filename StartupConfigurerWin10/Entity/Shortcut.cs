e5731e7e2d7ce2f45f5df60da2ea00e4ee8a7f2f
sekiya9311 <sekiya9311@gmail.com>
2019-03-15 00:45:41 +0900

    Implement Shortcut class

--- StartupConfigurerWin10/Entity/Shortcut.cs
+++ StartupConfigurerWin10/Entity/Shortcut.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace StartupConfigurerWin10.Entity
 {
+    /// <summary>
+    /// WSHでのショートカット情報を表します。
+    /// </summary>
+    /// <remarks>
+    /// HACK: とりあえず使いそうなやつのみ
+    /// </remarks>
     class Shortcut
     {
+        /// <summary> ショートカットのアイコンのパスを取得、設定します。 </summary>
+        public string IconLocation { get; set; }
+        /// <summary> ショートカットのリンク先のパスを取得、設定します。 </summary>
+        public string TargetPath { get; set; }
+        /// <summary> ショートカットの実行時の引数を取得、設定します。 </summary>
+        public string Arguments { get; set; }
+        /// <summary> 作業フォルダのパスを取得、設定します。 </summary>
+        public string WorkingDirectory { get; set; }
+        /// <summary> 実行時のウィンドウスタイルを取得、設定します。 </summary>
+        public WindowStyle WindowStyle { get; set; }
+        /// <summary> ショートカットのコメントを取得、設定します。 </summary>
+        public string Description { get; set; }
+    }
+
+    /// <summary>
+    /// ウィンドウのスタイルを表します。
+    /// </summary>
+    enum WindowStyle
+    {
+        /// <summary> 通常状態を表します。 </summary>
+        Normal = 1,
+        /// <summary> 最大化状態を表します。 </summary>
+        Maximum = 3,
+        /// <summary> 最小化状態を表します。 </summary>
+        Minimum = 7
     }
 }
