b24501fb7ef52e26fc88e2491973bafe2b45cbf6
sekiya9311 <sekiya9311@gmail.com>
2019-03-18 23:58:51 +0900

    Add Shotcut adapter class

---
+++ StartupConfigurerWin10/Entity/ShortcutAdapter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartupConfigurerWin10.Entity
+{
+    class ShortcutAdapter : IShortcut
+    {
+        private IWshRuntimeLibrary.IWshShortcut _wshShortcut;
+
+        public ShortcutAdapter(IWshRuntimeLibrary.IWshShortcut wshShortcut)
+        {
+            if (wshShortcut is null)
+            {
+                throw new ArgumentNullException(nameof(wshShortcut));
+            }
+            _wshShortcut = wshShortcut;
+        }
+
+        public string IconLocation
+        {
+            get => _wshShortcut.IconLocation;
+            set => _wshShortcut.IconLocation = value;
+        }
+        public string TargetPath
+        {
+            get => _wshShortcut.TargetPath;
+            set => _wshShortcut.TargetPath = value;
+        }
+        public string Arguments
+        {
+            get => _wshShortcut.Arguments;
+            set => _wshShortcut.Arguments = value;
+        }
+        public string WorkingDirectory
+        {
+            get => _wshShortcut.WorkingDirectory;
+            set => _wshShortcut.WorkingDirectory = value;
+        }
+        public WindowStyle WindowStyle
+        {
+            get => (WindowStyle)_wshShortcut.WindowStyle;
+            set => _wshShortcut.WindowStyle = (int)value;
+        }
+        public string Description
+        {
+            get => _wshShortcut.Description;
+            set => _wshShortcut.Description = value;
+        }
+    }
+}
