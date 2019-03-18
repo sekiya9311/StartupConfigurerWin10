3080d8e95c283fe2f4318da7f0cef11d8f8ef8c6
sekiya9311 <sekiya9311@gmail.com>
2019-03-19 00:49:41 +0900

    Implements ShorcutUtil

--- StartupConfigurerWin10/Util/ShortcutUtil.cs
+++ StartupConfigurerWin10/Util/ShortcutUtil.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 using StartupConfigurerWin10.Entity;
 
+// TODO: 例外処理とか解放処理とかは後でがんばろー
+
 namespace StartupConfigurerWin10.Util
 {
     class ShortcutUtil
@@ -15,16 +17,80 @@ namespace StartupConfigurerWin10.Util
             "Programs",
             "Startup");
 
+        /// <summary>
+        /// 現在のスタートアップショートカットをシーケンスで返します。
+        /// </summary>
+        /// <returns>現在のスタートアップショートカットのシーケンス</returns>
         public static IEnumerable<IShortcut> GetCurrentStartup()
         {
-            // TODO: impl
-            throw new NotImplementedException();
+            var shell = new IWshRuntimeLibrary.WshShell();
+
+            var startupFiles = System.IO.Directory.EnumerateFiles(StartupPath);
+            foreach (var filePath in startupFiles)
+            {
+                if (!(shell.CreateShortcut(filePath) is IWshRuntimeLibrary.IWshShortcut wshShortcut))
+                {
+                    continue;
+                }
+                
+                yield return ConvertIWshShortcutToIShortcut(wshShortcut);
+            }
         }
 
+        /// <summary>
+        /// 引数のショートカットをスタートアップフォルダに作成します。
+        /// </summary>
+        /// <param name="shortcuts">スタートアップフォルダ保存対象のショートカット</param>
         public static void SaveStartup(IEnumerable<IShortcut> shortcuts)
         {
-            // TODO: impl
-            // delete & insert ???
+            var curStartupFiles = System.IO.Directory.EnumerateFiles(StartupPath);
+            foreach (var filePath in curStartupFiles)
+            {
+                System.IO.File.Delete(filePath);
+            }
+
+            foreach (var shortcut in shortcuts)
+            {
+                var wshShortcut = ConvertIShortcutToIWshShortcut(shortcut);
+                wshShortcut.Save();
+                System.Runtime.InteropServices.Marshal.FinalReleaseComObject(wshShortcut);
+            }
+        }
+
+        /// <summary>
+        /// WSHのIWshShortcutから自作IShortcutのインスタンスを生成し返します。
+        /// </summary>
+        /// <param name="wshShortcut">コピー対象のWSH版ショートカットオブジェクト</param>
+        /// <returns>自作ショートカットオブジェクト</returns>
+        public static IShortcut ConvertIWshShortcutToIShortcut(IWshRuntimeLibrary.IWshShortcut wshShortcut)
+        {
+            var ret = new Shortcut()
+            {
+                IconLocation = wshShortcut.IconLocation,
+                TargetPath = wshShortcut.TargetPath,
+                Arguments = wshShortcut.Arguments,
+                WorkingDirectory = wshShortcut.WorkingDirectory,
+                WindowStyle = (WindowStyle)wshShortcut.WindowStyle,
+                Description = wshShortcut.Description,
+                FullName = wshShortcut.FullName
+            };
+
+            System.Runtime.InteropServices.Marshal.FinalReleaseComObject(wshShortcut);
+
+            return ret;
+        }
+
+        /// <summary>
+        /// 自作IShortcutのインスタンスからWSHのIWshShortcutのインスタンスを生成し返します。
+        /// </summary>
+        /// <param name="shortcut">コピー対象の自作ショートカットオブジェクト</param>
+        /// <returns>WSH版ショートカットオブジェクト</returns>
+        public static IWshRuntimeLibrary.IWshShortcut ConvertIShortcutToIWshShortcut(IShortcut shortcut)
+        {
+            var shell = new IWshRuntimeLibrary.WshShell();
+
+            var ret = shell.CreateShortcut(shortcut.FullName) as IWshRuntimeLibrary.IWshShortcut;
+            return ret;
         }
     }
 }
