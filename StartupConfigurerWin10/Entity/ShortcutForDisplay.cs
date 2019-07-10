30c52ca278dabb8c693bf17c58092143d354ff4e
sekiya9311 <sekiya9311@gmail.com>
2019-07-11 01:00:02 +0900

    Modify view

--- StartupConfigurerWin10/Entity/ShortcutForDisplay.cs
+++ StartupConfigurerWin10/Entity/ShortcutForDisplay.cs
@@ -8,7 +8,7 @@ using StartupConfigurerWin10.Base;
 
 namespace StartupConfigurerWin10.Entity
 {
-    public class ShortcutForDisplay : BindingBase, IShortcut
+    public class ShortcutForDisplay : IShortcut
     {
         private IShortcut _shortcut;
 
@@ -17,99 +17,14 @@ namespace StartupConfigurerWin10.Entity
             _shortcut = shortcut;
         }
 
-        public string IconLocation
-        {
-            get => _shortcut.IconLocation;
-            set
-            {
-                if (_shortcut.IconLocation != value)
-                {
-                    _shortcut.IconLocation = value;
-                    NotifyPropertyChanged(nameof(IconLocation));
-                }
-            }
-        }
-        public string TargetPath
-        {
-            get => _shortcut.TargetPath;
-            set
-            {
-                if (_shortcut.TargetPath != value)
-                {
-                    _shortcut.TargetPath = value;
-                    NotifyPropertyChanged(nameof(TargetPath));
-                }
-            }
-        }
-        public string Arguments
-        {
-            get => _shortcut.Arguments;
-            set
-            {
-                if (_shortcut.Arguments != value)
-                {
-                    _shortcut.Arguments = value;
-                    NotifyPropertyChanged(nameof(Arguments));
-                }
-            }
-        }
-        public string WorkingDirectory
-        {
-            get => _shortcut.WorkingDirectory;
-            set
-            {
-                if (_shortcut.WorkingDirectory != value)
-                {
-                    _shortcut.WorkingDirectory = value;
-                    NotifyPropertyChanged(nameof(WorkingDirectory));
-                }
-            }
-        }
-        public WindowStyle WindowStyle
-        {
-            get => _shortcut.WindowStyle;
-            set
-            {
-                if (_shortcut.WindowStyle != value)
-                {
-                    _shortcut.WindowStyle = value;
-                    NotifyPropertyChanged(nameof(WindowStyle));
-                }
-            }
-        }
-        public string Description
-        {
-            get => _shortcut.Description;
-            set
-            {
-                if (_shortcut.Description != value)
-                {
-                    _shortcut.Description = value;
-                    NotifyPropertyChanged(nameof(Description));
-                }
-            }
-        }
-        public string FullName
-        {
-            get => _shortcut.FullName;
-            set
-            {
-                if (_shortcut.FullName != value)
-                {
-                    _shortcut.FullName = value;
-                    NotifyPropertyChanged(nameof(FullName));
-                }
-            }
-        }
+        public string IconLocation { get => _shortcut.IconLocation; set => _shortcut.IconLocation = value; }
+        public string TargetPath { get => _shortcut.TargetPath; set => _shortcut.TargetPath = value; }
+        public string Arguments { get => _shortcut.Arguments; set => _shortcut.Arguments = value; }
+        public string WorkingDirectory { get => _shortcut.WorkingDirectory; set => _shortcut.WorkingDirectory = value; }
+        public WindowStyle WindowStyle { get => _shortcut.WindowStyle; set => _shortcut.WindowStyle = value; }
+        public string Description { get => _shortcut.Description; set => _shortcut.Description = value; }
+        public string FullName { get => _shortcut.FullName; set => _shortcut.FullName = value; }
 
-        private bool _isSelected;
-        public bool IsSelected
-        {
-            get => _isSelected;
-            set
-            {
-                CheckNeedNotifyPropertyChanged(ref _isSelected, value);
-            }
-        }
+        public string FileName => System.IO.Path.GetFileNameWithoutExtension(_shortcut.FullName);
     }
 }
--- StartupConfigurerWin10/ViewModel/MainWindowViewModel.cs
+++ StartupConfigurerWin10/ViewModel/MainWindowViewModel.cs
@@ -4,28 +4,33 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Reactive.Bindings;
 
 using StartupConfigurerWin10.Base;
+using StartupConfigurerWin10.Entity;
 using StartupConfigurerWin10.Model;
 
 namespace StartupConfigurerWin10.ViewModel
 {
-    public class MainWindowViewModel : BindingBase
+    public class MainWindowViewModel
     {
-        public IMainWindowModel Model { get; }
+        private IMainWindowModel _model;
 
-        public CommandBase AddCommand { get; }
+        public ReactiveCollection<ShortcutForDisplay> StartupShortcuts { get; }
 
-        public CommandBase RemoveCommand { get; }
+        public ReactivePropertySlim<IShortcut> SelectedShortcut { get; }
 
-        public CommandBase SaveCommand { get; }
+        public ReactiveCommand AddCommand { get; }
+
+        public ReactiveCommand RemoveCommand { get; }
+
+        public ReactiveCommand SaveCommand { get; }
 
         public MainWindowViewModel(IMainWindowModel model)
         {
-            this.Model = model;
-            this.AddCommand = new CommandBase(this.Model.AddStartup);
-            this.RemoveCommand = new CommandBase(this.Model.Remove);
-            this.SaveCommand = new CommandBase(this.Model.Save);
+            _model = model;
+
+
         }
 
         public MainWindowViewModel() : this(new MainWindowModel()) { }
