180e20bdd18fa3d02cde5b3f690fd4c0a4a2b4bf
sekiya9311 <sekiya9311@gmail.com>
2019-07-13 22:47:12 +0900

    Add SelectExecuteFileService

---
+++ StartupConfigurerWin10/Model/Interface/ISelectExecuteFileService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartupConfigurerWin10.Model
+{
+    interface ISelectExecuteFileService
+    {
+        string SelectExecuteFile();
+        IEnumerable<string> SelectExecuteFiles();
+    }
+}
---
+++ StartupConfigurerWin10/Model/SelectExecuteFileServiceUseDialog.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Win32;
+
+namespace StartupConfigurerWin10.Model
+{
+    class SelectExecuteFileServiceUseDialog : ISelectExecuteFileService
+    {
+        public string SelectExecuteFile()
+        {
+            var selectedFiles = SelectUseDialog(false);
+
+            return selectedFiles.DefaultIfEmpty(string.Empty).First();
+        }
+
+        public IEnumerable<string> SelectExecuteFiles()
+        {
+            var selectedFiles = SelectUseDialog(true);
+
+            return selectedFiles;
+        }
+
+        private IEnumerable<string> SelectUseDialog(bool multiSelect)
+        {
+            var dialog = new OpenFileDialog()
+            {
+                AddExtension = true,
+                CheckFileExists = true,
+                CheckPathExists = true,
+                Multiselect = multiSelect,
+                DefaultExt = "実行ファイル|*.exe"
+            };
+            
+            var selected = dialog.ShowDialog() ?? false;
+            if (!selected)
+            {
+                return Enumerable.Empty<string>();
+            }
+            
+            return dialog.FileNames.Clone() as IEnumerable<string>;
+        }
+    }
+}
