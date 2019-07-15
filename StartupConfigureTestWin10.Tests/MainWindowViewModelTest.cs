084ff69815440378f94707eec76ce6379b84cadb
sekiya9311 <sekiya9311@gmail.com>
2019-07-16 00:00:18 +0900

    Add MainWindowViewModel Test

--- StartupConfigureTestWin10.Tests/MainWindowViewModelTest.cs
+++ StartupConfigureTestWin10.Tests/MainWindowViewModelTest.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using StartupConfigurerWin10;
+using StartupConfigurerWin10.Entity;
 using StartupConfigurerWin10.Model;
 using StartupConfigurerWin10.ViewModel;
 
@@ -15,24 +16,66 @@ namespace StartupConfigureTestWin10.Tests
 {
     public class MainWindowViewModelTest
     {
+        private readonly IEnumerable<IShortcut> _samples = new[]
+        {
+            new Shortcut()
+            {
+                Arguments = "arg1",
+                Description = "desc1",
+                FullName = "full1",
+                IconLocation = "icon1",
+                TargetPath = "target1",
+                WindowStyle = WindowStyle.Normal,
+                WorkingDirectory = "work1"
+            },
+            new Shortcut()
+            {
+                Arguments = "arg2",
+                Description = "desc2",
+                FullName = "full2",
+                IconLocation = "icon2",
+                TargetPath = "target2",
+                WindowStyle = WindowStyle.Maximum,
+                WorkingDirectory = "work2"
+            }
+        };
+
         [Fact]
         public void Test()
         {
-            // TODO: あとでちゃんと書く
-            //var modelMock = new Mock<IMainWindowModel>();
-            //var vm = new MainWindowViewModel(modelMock.Object);
+            var modelMock = new Mock<IMainWindowModel>();
+            modelMock
+                .Setup(m => m.GetStartupShortcuts())
+                .Returns(_samples.Take(1));
+            modelMock
+                .Setup(m => m.NewStartupShortcut())
+                .Returns(_samples.Skip(1));
 
-            //Assert.True(vm.AddCommand.CanExecute());
-            //vm.AddCommand.Execute(null);
-            //modelMock.Verify(m => m.AddStartup(), Times.Once);
+            using (var vm = new MainWindowViewModel(modelMock.Object))
+            {
+                Assert.Equal(-1, vm.SelectedShortcutIndex.Value);
+                Assert.Equal(_samples.Take(1), vm.StartupShortcuts);
 
-            //Assert.True(vm.RemoveCommand.CanExecute());
-            //vm.RemoveCommand.Execute(null);
-            //modelMock.Verify(m => m.Remove(), Times.Once);
+                vm.AddCommand.Execute();
+                Assert.Equal(_samples, vm.StartupShortcuts);
 
-            //Assert.True(vm.SaveCommand.CanExecute());
-            //vm.SaveCommand.Execute(null);
-            //modelMock.Verify(m => m.Save(), Times.Once);
+                vm.SelectedShortcutIndex.Value = 0;
+                vm.RemoveCommand.Execute();
+                Assert.Equal(_samples.Skip(1), vm.StartupShortcuts);
+                Assert.Equal(-1, vm.SelectedShortcutIndex.Value);
+
+                vm.SaveCommand.Execute();
+                modelMock.Verify(
+                    m => m.DeleteStartupShortcuts(
+                        It.Is<IEnumerable<IShortcut>>(
+                            a => a.SequenceEqual(_samples.Take(1)))),
+                    Times.Once());
+                modelMock.Verify(
+                    m => m.SaveStartupShortcuts(
+                        It.Is<IEnumerable<IShortcut>>(
+                            a => a.SequenceEqual(_samples.Skip(1)))),
+                    Times.Once());
+            }
         }
     }
 }
