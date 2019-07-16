f96a6945b59428e0de143ed1d43644b93ed175c3
sekiya9311 <sekiya9311@gmail.com>
2019-07-16 22:43:19 +0900

    Refactor & Fix MainWindowViewModel test

--- StartupConfigurerWin10.Tests/MainWindowViewModelTest.cs
+++ StartupConfigurerWin10.Tests/MainWindowViewModelTest.cs
@@ -41,40 +41,94 @@ namespace StartupConfigurerWin10.Tests
         };
 
         [Fact]
-        public void Test()
+        public void ConstructorTest()
         {
             var modelMock = new Mock<IMainWindowModel>();
             modelMock
                 .Setup(m => m.GetStartupShortcuts())
-                .Returns(_samples.Take(1));
-            modelMock
-                .Setup(m => m.NewStartupShortcut())
-                .Returns(_samples.Skip(1));
+                .Returns(_samples);
 
             using (var vm = new MainWindowViewModel(modelMock.Object))
             {
                 Assert.Equal(-1, vm.SelectedShortcutIndex.Value);
-                Assert.Equal(_samples.Take(1), vm.StartupShortcuts);
+                Assert.Equal(_samples, vm.StartupShortcuts.OfType<IShortcut>());
+            }
+        }
 
-                vm.AddCommand.Execute();
+        [Fact]
+        public void AddCommandTest()
+        {
+            var modelMock = new Mock<IMainWindowModel>();
+            modelMock
+                .Setup(m => m.NewStartupShortcut())
+                .Returns(_samples);
+
+            using (var vm = new MainWindowViewModel(modelMock.Object))
+            {
+                vm.AddCommand.Execute(null);
+
                 Assert.Equal(_samples, vm.StartupShortcuts);
+            }
+        }
+
+        [Fact]
+        public void RemoveCommandTest()
+        {
+            var modelMock = new Mock<IMainWindowModel>();
+            modelMock
+                .Setup(m => m.GetStartupShortcuts())
+                .Returns(_samples);
+
+            using (var vm = new MainWindowViewModel(modelMock.Object))
+            {
+                vm.SelectedShortcutIndex.Subscribe(val =>
+                {
+                    // RemoveCommand で -1 に書き換えられる
+                    if (val == -1) return;
+
+                    vm.RemoveCommand.Execute(null);
+
+                    Assert.Equal(_samples.Skip(1), vm.StartupShortcuts);
+                    Assert.Equal(-1, vm.SelectedShortcutIndex.Value);
+                });
 
                 vm.SelectedShortcutIndex.Value = 0;
-                vm.RemoveCommand.Execute();
-                Assert.Equal(_samples.Skip(1), vm.StartupShortcuts);
-                Assert.Equal(-1, vm.SelectedShortcutIndex.Value);
+            }
+        }
 
-                vm.SaveCommand.Execute();
-                modelMock.Verify(
-                    m => m.DeleteStartupShortcuts(
-                        It.Is<IEnumerable<IShortcut>>(
-                            a => a.SequenceEqual(_samples.Take(1)))),
-                    Times.Once());
-                modelMock.Verify(
-                    m => m.SaveStartupShortcuts(
-                        It.Is<IEnumerable<IShortcut>>(
-                            a => a.SequenceEqual(_samples.Skip(1)))),
-                    Times.Once());
+        [Fact]
+        public void SaveCommandTest()
+        {
+            var modelMock = new Mock<IMainWindowModel>();
+            modelMock
+                .Setup(m => m.GetStartupShortcuts())
+                .Returns(_samples);
+
+            using (var vm = new MainWindowViewModel(modelMock.Object))
+            {
+                // 削除 -> 保存
+
+                vm.SelectedShortcutIndex.Subscribe(val =>
+                {
+                    // RemoveCommand で -1 に書き換えられる
+                    if (val == -1) return;
+
+                    vm.RemoveCommand.Execute(null);
+                    vm.SaveCommand.Execute(null);
+
+                    modelMock.Verify(
+                        m => m.DeleteStartupShortcuts(
+                            It.Is<IEnumerable<IShortcut>>(
+                                a => a.SequenceEqual(_samples.Take(1)))),
+                        Times.Once());
+                    modelMock.Verify(
+                        m => m.SaveStartupShortcuts(
+                            It.Is<IEnumerable<IShortcut>>(
+                                a => a.SequenceEqual(_samples.Skip(1)))),
+                        Times.Once());
+                });
+
+                vm.SelectedShortcutIndex.Value = 0;
             }
         }
     }
